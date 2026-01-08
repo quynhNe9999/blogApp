55206d19f532692046b87d5bff7295774b93878f
DESKTOP-O58ED60\tdquy <quynhtd@oneempower.com>
2026-01-08 10:17:22 +0700

    FE: update

--- src/main/webapp/app/entities/menu.tsx
+++ src/main/webapp/app/entities/menu.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import MenuItem from 'app/shared/layout/menus/menu-item'; // eslint-disable-line
-
 const EntitiesMenu = () => {
   return <>{/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}</>;
 };
